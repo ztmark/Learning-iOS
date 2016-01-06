5ca2fa158167b9aa44a8eafa3c88dbe68595d2a4
Mark <shuyumark@gmail.com>
2016-01-06 15:17:09 +0800

    update

--- Hypnosister/Hypnosister/HypnosisView.m
+++ Hypnosister/Hypnosister/HypnosisView.m
@@ -25,10 +25,15 @@
     center.x = bounds.origin.x + bounds.size.width / 2.0;
     center.y = bounds.origin.y + bounds.size.height / 2.0;
 
-    float radius = (MIN(bounds.size.width, bounds.size.height) / 2.0);
+    float maxRadius = hypot(bounds.size.width, bounds.size.height) / 2.0;
 
     UIBezierPath *path = [[UIBezierPath alloc] init];
-    [path addArcWithCenter:center radius:radius startAngle:0 endAngle:M_PI * 2 clockwise:YES];
+
+    for (float currentRadius = maxRadius; currentRadius > 0; currentRadius -= 20) {
+        [path moveToPoint:CGPointMake(center.x + currentRadius, center.y)];
+        [path addArcWithCenter:center radius:currentRadius startAngle:0 endAngle:M_PI * 2 clockwise:YES];
+    }
+
     path.lineWidth = 10;
 
     [[UIColor lightGrayColor] setStroke];
