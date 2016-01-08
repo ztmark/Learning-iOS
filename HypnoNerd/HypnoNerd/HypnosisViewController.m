0d4f0c719b469ea8fb28b5cd65f99831374d4039
Mark <shuyumark@gmail.com>
2016-01-08 16:31:30 +0800

    update

--- HypnoNerd/HypnoNerd/HypnosisViewController.m
+++ HypnoNerd/HypnoNerd/HypnosisViewController.m
@@ -10,11 +10,25 @@
 #import "HypnosisViewController.h"
 
 
+@interface HypnosisViewController () <UITextFieldDelegate>
+@end
+
 @implementation HypnosisViewController
 
 
 - (void)loadView {
-    HypnosisView *backgroundView = [[HypnosisView alloc] init];
+    CGRect frame = [UIScreen mainScreen].bounds;
+    HypnosisView *backgroundView = [[HypnosisView alloc] initWithFrame:frame];
+
+    CGRect textFieldRect = CGRectMake(40, 70, 240, 30);
+    UITextField *textField = [[UITextField alloc] initWithFrame:textFieldRect];
+
+    textField.borderStyle = UITextBorderStyleRoundedRect;
+    textField.placeholder = @"Hypnotize me";
+    textField.returnKeyType = UIReturnKeyDone;
+    textField.delegate = self;
+    [backgroundView addSubview:textField];
+
     self.view = backgroundView;
 }
 
@@ -23,6 +37,35 @@
     NSLog(@"HypnosisViewController loaded its view");
 }
 
+- (BOOL)textFieldShouldReturn:(UITextField *)textField {
+//    NSLog(@"%@", textField.text);
+    [self drawHypnoticMessage:textField.text];
+    textField.text = @"";
+    [textField resignFirstResponder];
+    return YES;
+}
+
+- (void)drawHypnoticMessage:(NSString *)message {
+    for (int i = 0; i < 20; ++i) {
+        UILabel *messageLabel = [[UILabel alloc] init];
+        messageLabel.backgroundColor = [UIColor clearColor];
+        messageLabel.textColor = [UIColor blueColor];
+        messageLabel.text = message;
+
+        [messageLabel sizeToFit];
+        int width = (int) (self.view.bounds.size.width - messageLabel.bounds.size.width);
+        int x = arc4random() % width;
+
+        int height = (int) (self.view.bounds.size.height - messageLabel.bounds.size.height);
+        int y = arc4random() % height;
+
+        CGRect frame = messageLabel.frame;
+        frame.origin = CGPointMake(x, y);
+        messageLabel.frame = frame;
+
+        [self.view addSubview:messageLabel];
+    }
+}
 
 - (instancetype)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
