a9ea76474d29c0ec6882388941fa1b61892cb2fc
Mark <shuyumark@gmail.com>
2016-01-12 14:49:40 +0800

    update

--- TouchTracker/TouchTracker/MKZDrawView.m
+++ TouchTracker/TouchTracker/MKZDrawView.m
@@ -16,6 +16,8 @@
 @property (nonatomic, strong) NSMutableDictionary *linesInProgress;
 @property (nonatomic, strong) NSMutableArray *finishedLines;
 
+@property (nonatomic, weak) MKZLine *selectedLine;
+
 @end
 
 
@@ -30,9 +32,57 @@
         self.finishedLines = [[NSMutableArray alloc] init];
         self.backgroundColor = [UIColor grayColor];
         self.multipleTouchEnabled = YES;
+
+        UITapGestureRecognizer *doubleTapRecognizer = [[UITapGestureRecognizer alloc] initWithTarget:self
+                                                                                              action:@selector(doubleTap:)];
+        doubleTapRecognizer.numberOfTapsRequired = 2;
+        doubleTapRecognizer.delaysTouchesBegan = YES;
+        [self addGestureRecognizer:doubleTapRecognizer];
+
+        UITapGestureRecognizer *tapRecognizer = [[UITapGestureRecognizer alloc] initWithTarget:self
+                                                                                        action:@selector(tap:)];
+        tapRecognizer.delaysTouchesBegan = YES;
+        [tapRecognizer requireGestureRecognizerToFail:doubleTapRecognizer];
+        [self addGestureRecognizer:tapRecognizer];
     }
 
     return self;
+}
+
+- (void)tap:(UITapGestureRecognizer *)tap {
+    NSLog(@"Recognized tap");
+    CGPoint point = [tap locationInView:self];
+    self.selectedLine = [self lineAtPoint:point];
+
+    if (self.selectedLine) {
+        [self becomeFirstResponder];
+        UIMenuController *menu = [UIMenuController sharedMenuController];
+        UIMenuItem *deleteItem = [[UIMenuItem alloc] initWithTitle:@"Delete" action:@selector(deleteLine:)];
+        menu.menuItems = @[deleteItem];
+        [menu setTargetRect:CGRectMake(point.x, point.y, 2, 2) inView:self];
+        [menu setMenuVisible:YES animated:YES];
+    } else {
+        [[UIMenuController sharedMenuController] setMenuVisible:NO animated:YES];
+    }
+
+    [self setNeedsDisplay];
+}
+
+- (void)deleteLine:(id)deleteLine {
+    [self.finishedLines removeObject:self.selectedLine];
+    [self setNeedsDisplay];
+}
+
+- (BOOL)canBecomeFirstResponder {
+    return YES;
+}
+
+
+- (void)doubleTap:(UIGestureRecognizer *)gr {
+    NSLog(@"Recognized double tap");
+    [self.linesInProgress removeAllObjects];
+    [self.finishedLines removeAllObjects];
+    [self setNeedsDisplay];
 }
 
 
@@ -49,6 +99,10 @@
     for (NSValue *key in self.linesInProgress) {
         [self strokeLine:self.linesInProgress[key]];
     }
+    if (self.selectedLine) {
+        [[UIColor greenColor] set];
+        [self strokeLine:self.selectedLine];
+    }
 }
 
 - (void)strokeLine:(MKZLine *)line {
@@ -60,6 +114,20 @@
     [bp stroke];
 }
 
+- (MKZLine *)lineAtPoint:(CGPoint)p {
+    for (MKZLine *line in self.finishedLines) {
+        CGPoint start = line.begin;
+        CGPoint end = line.end;
+        for (float i = 0.0; i <= 1.0; i += 0.05) {
+            float x = start.x + i * (end.x - start.x);
+            float y = start.y + i * (end.y - start.y);
+            if (hypot(x - p.x, y - p.y) < 20.0) {
+                return line;
+            }
+        }
+    }
+    return nil;
+}
 
 - (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
     NSLog(@"%@", NSStringFromSelector(_cmd));
