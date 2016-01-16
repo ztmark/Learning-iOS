033315b7a89080856653740e368106858ea39590
Mark <shuyumark@gmail.com>
2016-01-16 19:57:47 +0800

    update

--- Homepwner/Homepwner/ImageStore.m
+++ Homepwner/Homepwner/ImageStore.m
@@ -12,6 +12,8 @@
 
 @property (nonatomic, strong) NSMutableDictionary *dictionary;
 
+- (NSString *)imagePathForKey:(NSString *)key;
+
 @end
 
 
@@ -38,21 +40,52 @@
     self = [super init];
     if (self) {
         _dictionary = [[NSMutableDictionary alloc] init];
+        NSNotificationCenter *nc = [NSNotificationCenter defaultCenter];
+        [nc addObserver:self selector:@selector(clearCache:) name:UIApplicationDidReceiveMemoryWarningNotification object:nil];
     }
     return self;
+}
+
+- (void)clearCache:(NSNotification *)note {
+    NSLog(@"flushing %d images not out of the cache", [self.dictionary count]);
+    [self.dictionary removeAllObjects];
 }
 
 - (void)setImage:(UIImage *)image forKey:(NSString *)key {
 //    [self.dictionary setObject:image forKey:key];
     self.dictionary[key] = image;
+    NSString *imagePath = [self imagePathForKey:key];
+    NSData *data = UIImageJPEGRepresentation(image, 0.5);
+    [data writeToFile:imagePath atomically:YES];
 }
 
 - (UIImage *)imageForKey:(NSString *)key {
-    return self.dictionary[key];
+    UIImage *result = self.dictionary[key];
+    if (!result) {
+        NSString *imagePath = [self imagePathForKey:key];
+        result = [UIImage imageWithContentsOfFile:imagePath];
+        if (result) {
+            self.dictionary[key] = result;
+        } else {
+            NSLog(@"Error: unable to find %@", imagePath);
+        }
+    }
+    return result;
 }
 
 - (void)deleteImageForKey:(NSString *)key {
+    if (!key) {
+        return;
+    }
     [self.dictionary removeObjectForKey:key];
+    NSString *imagePath = [self imagePathForKey:key];
+    [[NSFileManager defaultManager] removeItemAtPath:imagePath error:nil];
+}
+
+- (NSString *)imagePathForKey:(NSString *)key {
+    NSArray *documentDirectories = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    NSString *documentDirectory = [documentDirectories firstObject];
+    return [documentDirectory stringByAppendingPathComponent:key];
 }
 
 
